24b9906b456d6fbd63e24520dc8fe98e7cd3a50d
rubianya <anyarat.an.03@gmail.com>
2026-08-26 18:25:19 +0700

    clean code

--- src/app/registrations/registrations-form/registrations-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-registrations-form',
-  imports: [],
-  templateUrl: './registrations-form.html',
-  styleUrl: '../../../styles/_registration-form.css',
-})
-export class RegistrationsForm {}
